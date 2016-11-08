c665f1db12b9e56dbc485529b4d148997a3dbda8
Dourp <erik.lindblom1@gmail.com>
2016-11-08 10:35:09 +0100

    så klar

---
+++ IinlämingsUppgiftDatabas2/IinlämingsUppgiftDatabas2/dbo/Tiggers/ContactNameUpdate.sql
@@ -0,0 +1,15 @@
+﻿CREATE TRIGGER [dbo].[ContactNameUpdate] 
+   ON  [dbo].[Customers] 
+   for UPDATE
+AS 
+BEGIN
+
+	SET NOCOUNT ON;
+
+	Insert into [dbo].[ContactNameChanges] ([CustomerID], [OldContactName], [NewContactName], [ChangedDate])
+		select i.CustomerID, d.ContactName, i.ContactName, GETDATE()
+			from inserted i
+				inner join deleted d on i.[CustomerID]=d.[CustomerID]
+			where d.[ContactName] <> i.[ContactName]
+
+END
