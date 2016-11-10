07fd5f687ee6d9bc4db8e01ea26765c330adade3
Dourp <erik.lindblom1@gmail.com>
2016-11-10 12:53:16 +0100

    så

--- IinlämingsUppgiftDatabas2/IinlämingsUppgiftDatabas2/dbo/Tables/ContactNameChanges.sql
+++ IinlämingsUppgiftDatabas2/IinlämingsUppgiftDatabas2/dbo/Tables/ContactNameChanges.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [dbo].[ContactNameChanges](
-    [ContactNameChangeID] [int] IDENTITY(1,1) NOT NULL,
+    [ContactNameChangesID] [int] IDENTITY(1,1) NOT NULL,
     [CustomerID] [nchar](5) NOT NULL,
     [OldContactName] [nvarchar](30) NOT NULL,
     [NewContactName] [nvarchar](30) NOT NULL,
     [ChangedDate] [datetime] NOT NULL,
     PRIMARY KEY CLUSTERED 
     (
-        [ContactNameChangeID] ASC
+        [ContactNameChangesID] ASC
     )
 )
 
